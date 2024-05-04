b0a962ca303d736683dadac19dc7141c7181bf36
Rishik Adhikari <adhikaririshik@gmail.com>
2024-05-04 19:39:29 -0400

    lint & format: flake8, black & pre-commit. updated some linting suggestions manually

--- app.py
+++ app.py
@@ -3,30 +3,32 @@ from fastapi import FastAPI, Response, status
 from models.fashionModel import fashion_classifier
 import torch
 
-#initialize the app
+# initialize the app
 app = FastAPI()
 
 # initialize the model
-model = fashion_classifier.FashioniModel( input_size = fashion_classifier.input_size,
-                        hidden_size = fashion_classifier.hidden_size,
-                        output_size = fashion_classifier.output_size
-                        )
+model = fashion_classifier.FashioniModel(
+    input_size=fashion_classifier.input_size,  # noqa: E501
+    hidden_size=fashion_classifier.hidden_size,
+    output_size=fashion_classifier.output_size,
+)
 
 # load the model weights
-model.load_state_dict(torch.load(f='models/fashionModel/fashionModel_params.pth'))
+model.load_state_dict(torch.load(f="models/fashionModel/fashionModel_params.pth"))
+
 
 # the api endpoint for prediction
 @app.get("/predict/{pixel_values}", status_code=status.HTTP_200_OK)
 async def main(pixel_values: str, response: Response):
-    pixel_values = np.array([ float(p.strip()) for p in pixel_values.split(',') ])
+    pixel_values = np.array([float(p.strip()) for p in pixel_values.split(",")])
     pixel_values = torch.tensor(pixel_values, dtype=torch.float32)
     if len(pixel_values) != 784:
         response.status_code = status.HTTP_400_BAD_REQUEST
-        return {'error': 'Invalid input, expected 784 pixel values'}
+        return {"error": "Invalid input, expected 784 pixel values"}
     pixel_values = pixel_values.view(1, 784)
     model.eval()
     with torch.inference_mode():
         output = model(pixel_values)
         predicted = torch.argmax(output)
         prob = output[0][predicted]
-    return {'predicted': predicted.item(), 'probability': prob.item()}
+    return {"predicted": predicted.item(), "probability": prob.item()}
--- models/fashionModel/fashion_classifier.py
+++ models/fashionModel/fashion_classifier.py
@@ -1,20 +1,21 @@
-import torch
 from torch import nn
 
 input_size = 784
 hidden_size = 10
 output_size = 10
 
+
 class FashioniModel(nn.Module):
-  def __init__(self,input_size, hidden_size, output_size):
 
-    super().__init__()
+    def __init__(self, input_size, hidden_size, output_size):
 
-    self.layer_stack = nn.Sequential(
-        nn.Flatten(),
-        nn.Linear(input_size, hidden_size),
-        nn.Linear(hidden_size, output_size)
+        super().__init__()
+
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input_size, hidden_size),
+            nn.Linear(hidden_size, output_size),
         )
 
-  def forward(self,x):
-    return self.layer_stack(x)
+    def forward(self, x):
+        return self.layer_stack(x)
